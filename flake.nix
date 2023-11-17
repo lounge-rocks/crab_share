3765ee1ae40087e98052902c6d3161b4213c9a67
MayNiklas <info@niklas-steffen.de>
2023-11-17 12:43:37 +0100

    greatly improve the flake setup

--- flake.nix
+++ flake.nix
@@ -1,36 +1,53 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        stdenv = pkgs.stdenv;
-        lib = pkgs.lib;
-      in
-      rec {
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
 
-        # `nix fmt'
-        formatter = pkgs.nixpkgs-fmt;
+  outputs = { self, nixpkgs, ... }:
+    let
+      # System types to support.
+      supportedSystems = [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
 
-        defaultPackage = packages.crab_share;
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-        packages = flake-utils.lib.flattenTree rec {
-          crab_share =
-            let manifest = (lib.importTOML ./Cargo.toml).package;
-            in pkgs.rustPlatform.buildRustPackage {
-              pname = manifest.name;
-              version = manifest.version;
-              src = lib.cleanSource self;
-              cargoLock = { lockFile = ./Cargo.lock; };
-              nativeBuildInputs = with pkgs;
-                lib.optionals stdenv.isLinux [ pkg-config ];
-              buildInputs = with pkgs; [ openssl ]; # TODO: check how to make this work on darwin?
-            };
-        };
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system:
+        import nixpkgs { inherit system; overlays = [ self.overlay ]; });
+    in
+    {
 
-      });
+      formatter = forAllSystems
+        (system: nixpkgsFor.${system}.nixpkgs-fmt);
+
+      overlay = final: prev: {
+
+        crab_share = with final;
+          let manifest = (pkgs.lib.importTOML ./Cargo.toml).package; in
+          pkgs.rustPlatform.buildRustPackage {
+            pname = manifest.name;
+            version = manifest.version;
+            src = lib.cleanSource self;
+            cargoLock = { lockFile = ./Cargo.lock; };
+            nativeBuildInputs = with pkgs;
+              lib.optionals stdenv.isLinux [ pkg-config ];
+            buildInputs = with pkgs; [ openssl ]; # TODO: check how to make this work on darwin?
+          };
+
+      };
+
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system}; in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [ cargo openssl ];
+          };
+        });
+
+      packages = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system}; in {
+          default = pkgs.crab_share;
+          crab_share = pkgs.crab_share;
+        });
+
+    };
+
 }
