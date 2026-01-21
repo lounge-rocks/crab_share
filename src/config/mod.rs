c4fb00650904a76d68fa55e89bfd9a4499abfa40
MayNiklas <info@niklas-steffen.de>
2026-01-21 09:40:02 +0100

    set default compression to deflated

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -120,7 +120,7 @@ impl PartialConfig {
             path: None,
             region: Some("eu-central-1".to_string()),
             credentials: None,
-            compression: Some(CompressionMethod::Zstd),
+            compression: Some(CompressionMethod::Deflated),
             zip_single_file: Some(false),
             purge: Some(false),
         }
